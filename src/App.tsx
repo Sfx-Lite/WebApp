cbfe5a792ea2253e3c5f237dda333ae280c44444
Bland Irony <blandirony@gmail.com>
2026-07-14 22:04:49 +0100

    :sparkles: Initialized project, setup tailwind utilities and setup routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router";
+import ChatLayout from "./layouts/Chat/ChatLayout";
+import HomeLayout from "./layouts/Home/HomeLayout";
+import History from "./pages/History";
+import Home from "./pages/Home";
+import Rates from "./pages/Rates";
+import Settings from "./pages/Settings";
+import SupportChat from "./pages/SupportChat";
+
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Routes>
+      <Route element={<HomeLayout />}>
+        <Route path="home" element={<Home />} />
+        <Route path="rates" element={<Rates />} />
+        <Route path="history" element={<History />} />
+        <Route path="settings" element={<Settings />} />
+      </Route>
+
+      <Route element={<ChatLayout />}>
+        <Route path="support" element={<SupportChat />} />
+      </Route>
+    </Routes>
   );
 }
 
