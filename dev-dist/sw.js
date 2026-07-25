9543b7cff29e3c0070024b3f001ae4fb19a2df0e
Faith <150615446+itsjustfaith@users.noreply.github.com>
2026-07-25 21:28:50 +0300

    Integrated API for pwd-change and user-profile-edit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-84250dca'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dt6beesabhg"
+    "revision": "0.5t17d2cb0uo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
